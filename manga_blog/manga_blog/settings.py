9eb161929633f26dcc62b5dd7095601c5e8f431b
Alfred Saker Kuate . k <72146099+alfred-saker@users.noreply.github.com>
2023-08-11 14:31:24 +0200

    settings

--- manga_blog/manga_blog/settings.py
+++ manga_blog/manga_blog/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 
-import django_heroku
-import dj_database_url
-
 from pathlib import Path
 from django.core.mail import send_mail
 
@@ -131,7 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # STATIC_ROOT = BASE_DIR / "staticfiles"
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
   os.path.join(BASE_DIR, 'static')
